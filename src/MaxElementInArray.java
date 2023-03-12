4d7e297d249f6a55065b04b91a1e18689ed7f4a5
Sainozhenko <dmytrosainozhenko@gmail.com>
2023-03-12 23:36:39 +0100

    Homework

--- src/MaxElementInArray.java
+++ src/MaxElementInArray.java
@@ -1,8 +1,10 @@
 public class MaxElementInArray {
     private int[] ints;
+
     MaxElementInArray(int[] ints) {
         this.ints = ints;
     }
+
     public int getMax() {
         int max = ints[0];
         for (int i : ints) {
